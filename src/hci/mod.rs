71cfb59ddc4e11975ba65f1a968e28c6adbb0c9f
Daniel Gallagher <pdanielgallagher@gmail.com>
2018-04-14 13:45:41 -0700

    Pull common HCI commands up into hci module

--- src/hci/mod.rs
+++ src/hci/mod.rs
@@ -11,6 +11,35 @@ fn msb_of(s: u16) -> u8 {
     (s >> 8) as u8
 }
 
+const MAX_HEADER_LENGTH: usize = 5;
+
+pub trait HciHeader {
+    const HEADER_LENGTH: usize;
+
+    fn new(op_code: u16, param_len: usize) -> Self;
+
+    // TODO(#42863): Simplify into_bytes into this form:
+    // fn into_bytes(&self) -> [u8; HEADER_LENGTH];
+    fn into_bytes(&self, buf: &mut [u8]);
+}
+
+pub trait Hci<E, Header> {
+    fn read_local_version_information(&mut self) -> nb::Result<(), E>;
+}
+
+impl<E, T, Header> Hci<E, Header> for T
+where
+    T: ::Controller<Error = E>,
+    Header: HciHeader,
+{
+    fn read_local_version_information(&mut self) -> nb::Result<(), E> {
+        let params = [];
+        let mut header = [0; MAX_HEADER_LENGTH];
+        Header::new(::opcode::LOCAL_VERSION_INFO, params.len()).into_bytes(&mut header);
+        self.write(&header, &params[..Header::HEADER_LENGTH])
+    }
+}
+
 pub mod uart {
     extern crate nb;
 
@@ -26,18 +55,18 @@ pub mod uart {
         Event(::Event),
     }
 
-    struct CommandHeader {
+    pub struct CommandHeader {
         op_code: u16,
         param_len: u8,
     }
 
-    pub trait Hci<E> {
-        fn read_local_version_information(&mut self) -> nb::Result<(), Error<E>>;
+    pub trait Hci<E>: super::Hci<E, CommandHeader> {
         fn read(&mut self) -> nb::Result<Packet, Error<E>>;
     }
 
-    const COMMAND_PACKET_HEADER_LENGTH: usize = 4;
-    impl CommandHeader {
+    impl super::HciHeader for CommandHeader {
+        const HEADER_LENGTH: usize = 4;
+
         fn new(op_code: u16, param_len: usize) -> CommandHeader {
             CommandHeader {
                 op_code: op_code,
@@ -45,28 +74,46 @@ pub mod uart {
             }
         }
 
-        fn into_bytes(&self) -> [u8; COMMAND_PACKET_HEADER_LENGTH] {
-            [
-                super::PACKET_TYPE_HCI_COMMAND,
-                super::lsb_of(self.op_code),
-                super::msb_of(self.op_code),
-                self.param_len,
-            ]
+        // TODO(#42863): Simplify into_bytes into this form:
+        // fn into_bytes(&self) -> [u8; COMMAND_PACKET_HEADER_LENGTH] {
+        //     [
+        //         super::PACKET_TYPE_HCI_COMMAND,
+        //         super::lsb_of(self.op_code),
+        //         super::msb_of(self.op_code),
+        //         self.param_len,
+        //     ]
+        // }
+
+        fn into_bytes(&self, buffer: &mut [u8]) {
+            buffer[0] = super::PACKET_TYPE_HCI_COMMAND;
+            buffer[1] = super::lsb_of(self.op_code);
+            buffer[2] = super::msb_of(self.op_code);
+            buffer[3] = self.param_len;
+        }
+    }
+
+    fn rewrap_error<E>(e: nb::Error<E>) -> nb::Error<Error<E>> {
+        match e {
+            nb::Error::WouldBlock => nb::Error::WouldBlock,
+            nb::Error::Other(err) => nb::Error::Other(Error::Comm(err)),
         }
     }
 
     fn read_event<E, T>(controller: &mut T) -> nb::Result<::Event, Error<E>>
     where
-        T: ::Controller<Error = Error<E>>,
+        T: ::Controller<Error = E>,
     {
         const MAX_EVENT_LENGTH: usize = 255;
         const PACKET_HEADER_LENGTH: usize = 1;
         const EVENT_PACKET_HEADER_LENGTH: usize = 3;
         const PARAM_LEN_BYTE: usize = 2;
-        let param_len = controller.peek(PARAM_LEN_BYTE)? as usize;
+
+        let param_len = controller.peek(PARAM_LEN_BYTE).map_err(rewrap_error)? as usize;
 
         let mut buf = [0; MAX_EVENT_LENGTH + EVENT_PACKET_HEADER_LENGTH];
-        controller.read_into(&mut buf[..EVENT_PACKET_HEADER_LENGTH + param_len])?;
+        controller
+            .read_into(&mut buf[..EVENT_PACKET_HEADER_LENGTH + param_len])
+            .map_err(rewrap_error)?;
 
         ::event::parse_event(::event::Packet(
             &buf[PACKET_HEADER_LENGTH..EVENT_PACKET_HEADER_LENGTH + param_len],
@@ -75,21 +122,13 @@ pub mod uart {
 
     impl<E, T> Hci<E> for T
     where
-        T: ::Controller<Error = Error<E>>,
+        T: ::Controller<Error = E>,
     {
         fn read(&mut self) -> nb::Result<Packet, Error<E>> {
-            match self.peek(0)? {
+            match self.peek(0).map_err(rewrap_error)? {
                 super::PACKET_TYPE_HCI_EVENT => Ok(Packet::Event(read_event(self)?)),
                 x => Err(nb::Error::Other(Error::BadPacketType(x))),
             }
-        }
-
-        fn read_local_version_information(&mut self) -> nb::Result<(), Error<E>> {
-            let params = [];
-            self.write(
-                &CommandHeader::new(::opcode::LOCAL_VERSION_INFO, params.len()).into_bytes(),
-                &params,
-            )
         }
     }
 }
